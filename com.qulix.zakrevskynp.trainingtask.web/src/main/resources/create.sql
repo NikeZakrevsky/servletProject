b15c5d1fa310f242ef59105bdb121d8d4a23f7ec
ZakrevskyNP <nikita290995@yandex.ru>
2017-04-10 14:33:41 +0300

    Issue #54. Отсутствует единообразие в sql скриптах создания БД.

--- com.qulix.zakrevskynp.trainingtask.web/src/main/resources/create.sql
+++ com.qulix.zakrevskynp.trainingtask.web/src/main/resources/create.sql
@@ -1,27 +1,27 @@
 create table projects (
   id integer identity primary key,
-  name varchar(25) NOT NULL,
-  shortname varchar(25) NOT NULL,
-  description varchar(50) NOT NULL
+  name varchar(25) not null,
+  shortname varchar(25) not null,
+  description varchar(50)
 );
 
 create table persons (
   id integer identity primary key,
-  fname varchar(25),
-  sname varchar(25),
-  lname varchar(25),
-  position varchar(25)
+  fname varchar(25) not null,
+  sname varchar(25) not null,
+  lname varchar(25) not null,
+  position varchar(25) not null
 );
 
 create table tasks (
   id integer identity primary key,
-  name varchar(50),
-  time int,
-  startDate date,
-  endDate date,
-  status varchar(50),
-  projectId INTEGER,
-  personId INTEGER,
+  name varchar(50) not null,
+  time integer not null,
+  startDate date not null,
+  endDate date not null,
+  status varchar(50) not null,
+  projectId integer,
+  personId integer,
   constraint project_fk foreign key(projectId) references projects(id)
   on delete set null
   on update cascade,
