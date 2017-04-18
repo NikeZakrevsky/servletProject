d8f54dc9ff7ebc28879774012bdf84d4c64333f2
ZakrevskyNP <nikita290995@yandex.ru>
2017-04-18 14:50:57 +0300

    Issue #4. Параметры подключения к базе захардкожены в классе ConnectionFactory

--- com.qulix.zakrevskynp.trainingtask.web/src/main/java/com/qulix/zakrevskynp/trainingtask/web/dao/ConnectionFactory.java
+++ com.qulix.zakrevskynp.trainingtask.web/src/main/java/com/qulix/zakrevskynp/trainingtask/web/dao/ConnectionFactory.java
@@ -2,6 +2,7 @@ package com.qulix.zakrevskynp.trainingtask.web.dao;
 
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -18,21 +19,35 @@ import com.qulix.zakrevskynp.trainingtask.web.dao.task.TasksDAOImpl;
 public class ConnectionFactory {
     private static Properties dbProperties;
     private static String url;
-
+    private static final String USER = "user";
+    private static final String PASSWORD = "password";
+    private static final String DRIVER_CLASS = "driverClass";
+    private static final String URL = "url";
+    private static final String JDBC_PROPERTIES_PATH = "com.qulix.zakrevskynp.trainingtask.web/src/main/resources/jdbc.properties";
+    private static final String DATABASE_CONNECTION_ERROR = "Can not connect";
     private static Logger logger = Logger.getLogger(TasksDAOImpl.class.getName());
-
+    private static final String READ_PROPERTIES_ERROR = "Can not read database properties";
+    
     private ConnectionFactory() {
     }
 
-    static {
+    private static void readDatabaseProperty() {
+        dbProperties = new Properties();
         try {
-            dbProperties = new Properties();
-            dbProperties.load(new FileInputStream("com.qulix.zakrevskynp.trainingtask.web/src/main/resources/jdbc.properties"));
-            Class.forName(dbProperties.getProperty("driverClass"));
-            url = dbProperties.getProperty("url");
+            dbProperties.load(new FileInputStream(JDBC_PROPERTIES_PATH));
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, READ_PROPERTIES_ERROR);
+        }
+    }
+
+    static {
+        readDatabaseProperty();
+        try {
+            Class.forName(dbProperties.getProperty(DRIVER_CLASS));
+            url = dbProperties.getProperty(URL);
         }
         catch (Exception e) {
-            logger.log(Level.SEVERE, "Can not connect");
+            logger.log(Level.SEVERE, DATABASE_CONNECTION_ERROR);
         }
     }
 
@@ -43,6 +58,6 @@ public class ConnectionFactory {
      * @throws SQLException throws while getting connection to database
      */
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(url, dbProperties.getProperty("user"), dbProperties.getProperty("password"));
+        return DriverManager.getConnection(url, dbProperties.getProperty(USER), dbProperties.getProperty(PASSWORD));
     }
 }
