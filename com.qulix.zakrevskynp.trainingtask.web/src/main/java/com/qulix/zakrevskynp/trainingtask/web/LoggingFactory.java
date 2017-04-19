980821ce0ef83fa56cb52786cacf44ef5ab3e9dd
ZakrevskyNP <nikita290995@yandex.ru>
2017-04-19 17:36:31 +0300

    logger configuration

--- com.qulix.zakrevskynp.trainingtask.web/src/main/java/com/qulix/zakrevskynp/trainingtask/web/LoggingFactory.java
+++ com.qulix.zakrevskynp.trainingtask.web/src/main/java/com/qulix/zakrevskynp/trainingtask/web/LoggingFactory.java
@@ -1,5 +1,9 @@
 package com.qulix.zakrevskynp.trainingtask.web;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 /**
@@ -9,12 +13,12 @@ public class LoggingFactory {
     private static final Logger LOGGER = Logger.getLogger(LoggingFactory.class.getName());
 
     static {
-        /*try {
-            //FileInputStream configFile = new FileInputStream("logger.properties");
-            //LogManager.getLogManager().readConfiguration(configFile);
+        try {
+            FileInputStream configFile = new FileInputStream("logger.properties");
+            LogManager.getLogManager().readConfiguration(configFile);
         } catch (IOException e) {
             LOGGER.log(Level.SEVERE, "Error while loading logger configuration");
-        }  */
+        }
     }
 
     public static Logger getLogger() {
