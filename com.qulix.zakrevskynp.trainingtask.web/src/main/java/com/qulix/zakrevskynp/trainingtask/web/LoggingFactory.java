06350f44459c4c94fb461f06df679dd1712cc4ef
ZakrevskyNP <nikita290995@yandex.ru>
2017-05-10 19:27:19 +0300

    Issue #138. Неверный способ загрузки файла с параметрами подключения к БД.

--- com.qulix.zakrevskynp.trainingtask.web/src/main/java/com/qulix/zakrevskynp/trainingtask/web/LoggingFactory.java
+++ com.qulix.zakrevskynp.trainingtask.web/src/main/java/com/qulix/zakrevskynp/trainingtask/web/LoggingFactory.java
@@ -1,7 +1,7 @@
 package com.qulix.zakrevskynp.trainingtask.web;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
@@ -14,14 +14,14 @@ import java.util.logging.Logger;
 public class LoggingFactory {
     private static final Logger LOGGER = Logger.getLogger(LoggingFactory.class.getName());
     private static final String LOAD_CONFIGURATION_ERROR = "Error while loading logger configuration";
+    private static final String LOGGER_PATH = "logger.properties";
 
     static {
         try {
-            FileInputStream configurationFile = new FileInputStream("com.qulix.zakrevskynp.trainingtask.web/src/main/resources/logger." +
-                    "properties");
-            LogManager.getLogManager().readConfiguration(configurationFile);
+            InputStream stream = LoggingFactory.class.getClassLoader().getResourceAsStream(LOGGER_PATH);
+            LogManager.getLogManager().readConfiguration(stream);
         } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Exception: " + LOAD_CONFIGURATION_ERROR + e);
+            LOGGER.log(Level.SEVERE, LOAD_CONFIGURATION_ERROR, e);
         }
     }
 
